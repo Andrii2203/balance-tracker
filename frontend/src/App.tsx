b86181a5577bfd9552b715df6c93b282baed129e
Andrii <shandrij1@gmail.com>
2025-06-24 17:30:56 +0200

    make a NewsList page component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DataViewer from "./components/DataViewer/DataViewer";
 import BottomNav, { NavItem } from "./components/BottomNav/BottomNav";
-import { FaChartBar, FaHome, FaUser } from 'react-icons/fa';
+import NewsList from "./components/NewsList/NewsList";
+import { title } from "process";
 
 function App() {
   const navItems: NavItem[] = [
@@ -12,13 +13,17 @@ function App() {
     { label: "Quotes", icon: '💬', to: "/quotes" },
     { label: "Chat", icon: '🗣️', to: "/chat" },
   ]
+
+  const newsData = [
+    {id: 1, title: 'Я купив курс', date: new Date("2025-06-23"),  summary:'Я вивів 1165 дол з Онфін та заплатив ці гроші за курс який дасть мені краще бачення ринку, щоб я міг точніше входити в ринок та отримувати більше прибутків'}
+  ]
   return (
     <Router>
       <div className="app-container">
         <Routes>
           <Route path="/" element={<div>Home Page</div>} />
           <Route path="/charts" element={<DataViewer sheetName="Arkusz1" />} />
-          <Route path="/news" element={<div>News Page</div>} />
+          <Route path="/news" element={<NewsList news={newsData}/>} />
           <Route path="/quotes" element={<div>Quotes Page</div>} />
           <Route path="/chat" element={<div>Chat Page</div>} />
         </Routes>
---
+++ frontend/src/components/NewsList/NewsList.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+interface NewsItem {
+    id: number;
+    date: Date;
+    title: string;
+    summary: string;
+}
+
+interface NewsListProps {
+    news: NewsItem[];
+}
+
+const NewsList: React.FC<NewsListProps> = ({ news }) => {
+    return (
+        <div>
+            {news.length === 0 ? (
+                <p>No news available</p>
+            ) : (
+                <ul>
+                    {news.map(item => (
+                        <li key={item.id}>
+                            <h3>{item.title}</h3>
+                            <span>{item.date.toLocaleDateString('uk-UA', {
+                                year: 'numeric',
+                                month: 'long',
+                                day: 'numeric'
+                            })}</span>
+                            <p>{item.summary}</p>
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    )
+}
+
+export default NewsList;
